48ca0b7d779cb2d3079835793d3a963d3cde094b
paul-shuvro <sudiptashuvro@gmail.com>
2026-01-29 20:25:00 +0600

    Initial Commit

---
+++ 1st Lab/gl-lines.py
@@ -0,0 +1,35 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+
+
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 1.0, 1.0)
+    glPointSize(3)
+    glBegin(GL_POINTS)
+    glVertex2f(250, 250)
+    glEnd()
+
+
+    glutSwapBuffers()
+
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(0, 0)
+wind = glutCreateWindow(b"Drawing Pixels")
+glutDisplayFunc(showScreen)
+
+glutMainLoop()
---
+++ README.md
@@ -0,0 +1,9 @@
+#Lab 1 (21/01/2026)
+
+activities:    
+-install py opengl   
+-draw points   
+-connect points   
+
+//@sudip
+
